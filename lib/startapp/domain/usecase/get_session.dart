a7a672ea1e960443da746cef25bd8d7fbfec1d6b
RODOLFO GAVILAN <bankenro..myhope@gmail.com>
2023-03-22 01:10:13 -0500

    refactor string param to void

--- lib/startapp/domain/usecase/get_session.dart
+++ lib/startapp/domain/usecase/get_session.dart
@@ -4,12 +4,12 @@ import 'package:user_cedtodo/startapp/data/network/failure.dart';
 import 'package:user_cedtodo/startapp/domain/repository/startapp_repository.dart';
 import 'package:user_cedtodo/startapp/domain/usecase/base_usecase.dart';
 
-class GetSessionUseCase implements BaseUseCase<String, dynamic> {
-  final StartappRepository _repository;
+class GetSessionUseCase implements BaseUseCase<void, dynamic> {
+  final StartappRepository _startappRepository;
 
-  GetSessionUseCase(this._repository);
+  GetSessionUseCase(this._startappRepository);
 
   @override
-  Future<Either<Failure, models.Session>> execute(String input) =>
-      _repository.getSession();
+  Future<Either<Failure, models.Session>> execute(void input) =>
+      _startappRepository.getSession();
 }
