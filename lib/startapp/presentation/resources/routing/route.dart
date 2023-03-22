422a3a300242de32ddc941933275ff20573ccbf5
RODOLFO GAVILAN <bankenro..myhope@gmail.com>
2023-03-22 01:09:30 -0500

    delete route redirect

--- lib/startapp/presentation/resources/routing/route.dart
+++ lib/startapp/presentation/resources/routing/route.dart
@@ -10,11 +10,10 @@ import 'package:user_cedtodo/home/presentation/home/home_view.dart';
 import 'package:user_cedtodo/home/presentation/home/pages/profile/profile_view.dart';
 import 'package:user_cedtodo/home/presentation/home/pages/restaurant/restaurant.dart';
 import 'package:user_cedtodo/home/presentation/home/pages/restaurants/restaurants_view.dart';
-import 'package:user_cedtodo/main.dart';
 import 'package:user_cedtodo/purchase/presentation/cart/cart_view.dart';
 import 'package:user_cedtodo/purchase/presentation/finalize_purchase/finalize_purchase_view.dart';
+import 'package:user_cedtodo/splash/app/splash_dependency_injection.dart';
 import 'package:user_cedtodo/splash/presentation/splash/splash_view.dart';
-import 'package:user_cedtodo/startapp/domain/usecase/get_session.dart';
 import 'package:user_cedtodo/startapp/presentation/resources/routing/routes.dart';
 
 final GlobalKey<NavigatorState> _rootNavigatorKey =
@@ -23,89 +22,89 @@ final GlobalKey<NavigatorState> _shellNavigatorKey =
     GlobalKey<NavigatorState>(debugLabel: 'shell');
 
 class AppRoute {
-  static GetSessionUseCase getSessionUseCase = getIt<GetSessionUseCase>();
   static final route = GoRouter(
-      navigatorKey: _rootNavigatorKey,
-      initialLocation: AppRoutes.splash,
-      routerNeglect: true,
-      debugLogDiagnostics: true,
-      routes: [
-        GoRoute(
-            path: AppRoutes.splash,
-            builder: (context, state) {
-              return const SplashView();
-            }),
-        GoRoute(
-            path: AppRoutes.login,
-            builder: (context, state) {
-              return const LoginView();
-            }),
-        GoRoute(
-            path: AppRoutes.register,
-            builder: (context, state) {
-              return const RegisterView();
-            }),
-        GoRoute(
-            path: AppRoutes.recoverPassword,
-            builder: (context, state) {
-              return const RecoverPasswordView();
-            }),
-        GoRoute(
-            path: AppRoutes.recoverPasswordConfirm,
-            builder: (context, state) {
-              return const RecoverPasswordConfirmView();
-            }),
-        ShellRoute(
-            navigatorKey: _shellNavigatorKey,
-            builder: (context, state, widget) {
-              return HomeView(widget: widget);
-            },
-            routes: [
-              GoRoute(
-                  path: AppRoutes.restaurants,
-                  builder: (context, state) {
-                    return const RestaurantsView();
-                  },
-                  routes: [
-                    GoRoute(
-                        path: '${AppRoutes.restaurant}/:id',
-                        builder: (context, state) {
-                          final restaurantId = state.params['id'];
-                          return RestaurantView(
-                            restaurantId: restaurantId,
-                          );
-                        })
-                  ]),
-              GoRoute(
-                  path: AppRoutes.profile,
-                  builder: (context, state) {
-                    return const ProfileView();
-                  }),
-            ]),
-        GoRoute(
-            path: AppRoutes.cart,
-            builder: (context, state) {
-              return const CartView();
-            }),
-        GoRoute(
-            path: AppRoutes.finalizePurchase,
-            builder: (context, state) {
-              return const FinalizePurchaseView();
-            }),
-        GoRoute(
-            path: AppRoutes.delivery,
-            builder: (context, state) {
-              return const DeliveryView();
-            }),
-        GoRoute(
-            path: AppRoutes.chat,
-            builder: (context, state) {
-              return const ChatView();
-            }),
-      ],
-      errorBuilder: (context, state) => _unDefinedRoute(),
-      redirect: (context, state) async {
-        final isLogged = (await getSessionUseCase.execute('input'))
+    navigatorKey: _rootNavigatorKey,
+    initialLocation: AppRoutes.splash,
+    routerNeglect: true,
+    debugLogDiagnostics: true,
+    routes: [
+      GoRoute(
+          path: AppRoutes.splash,
+          builder: (context, state) {
+            initSplashModule();
+            return const SplashView();
+          }),
+      GoRoute(
+          path: AppRoutes.login,
+          builder: (context, state) {
+            return const LoginView();
+          }),
+      GoRoute(
+          path: AppRoutes.register,
+          builder: (context, state) {
+            return const RegisterView();
+          }),
+      GoRoute(
+          path: AppRoutes.recoverPassword,
+          builder: (context, state) {
+            return const RecoverPasswordView();
+          }),
+      GoRoute(
+          path: AppRoutes.recoverPasswordConfirm,
+          builder: (context, state) {
+            return const RecoverPasswordConfirmView();
+          }),
+      ShellRoute(
+          navigatorKey: _shellNavigatorKey,
+          builder: (context, state, widget) {
+            return HomeView(widget: widget);
+          },
+          routes: [
+            GoRoute(
+                path: AppRoutes.restaurants,
+                builder: (context, state) {
+                  return const RestaurantsView();
+                },
+                routes: [
+                  GoRoute(
+                      path: '${AppRoutes.restaurant}/:id',
+                      builder: (context, state) {
+                        final restaurantId = state.params['id'];
+                        return RestaurantView(
+                          restaurantId: restaurantId,
+                        );
+                      })
+                ]),
+            GoRoute(
+                path: AppRoutes.profile,
+                builder: (context, state) {
+                  return const ProfileView();
+                }),
+          ]),
+      GoRoute(
+          path: AppRoutes.cart,
+          builder: (context, state) {
+            return const CartView();
+          }),
+      GoRoute(
+          path: AppRoutes.finalizePurchase,
+          builder: (context, state) {
+            return const FinalizePurchaseView();
+          }),
+      GoRoute(
+          path: AppRoutes.delivery,
+          builder: (context, state) {
+            return const DeliveryView();
+          }),
+      GoRoute(
+          path: AppRoutes.chat,
+          builder: (context, state) {
+            return const ChatView();
+          }),
+    ],
+    errorBuilder: (context, state) => _unDefinedRoute(),
+    /*redirect: (context, state) async {
+        final isLogged = (await getSessionUseCase.execute(null))
             .fold((l) => false, (r) => true);
         final routesNotLogged = state.subloc == AppRoutes.splash ||
             state.subloc == AppRoutes.login ||
@@ -114,7 +113,8 @@ class AppRoute {
             state.subloc == AppRoutes.recoverPasswordConfirm;
         if (!isLogged) return routesNotLogged ? null : AppRoutes.splash;
         return null;
-      });
+      }*/
+  );
 
   static _unDefinedRoute() {
     return Scaffold(
@@ -124,4 +124,8 @@ class AppRoute {
       body: const Center(child: Text('notFound')),
     );
   }
+}
+
+extension AppRouteExtension on BuildContext {
+  back() => canPop() ? pop() : go(AppRoutes.splash);
 }
